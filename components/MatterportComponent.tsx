1b5bf5cfadacd7d5fbae4daf04b3eccd72f44446
moritalous <moritalous@gmail.com>
2023-09-10 01:48:56 +0000

    Matterport SDKのサンプルを追加

---
+++ components/MatterportComponent.tsx
@@ -0,0 +1,149 @@
+'use client'
+import { useEffect, useRef, useState } from "react"
+
+import { MpSdk } from "@/types/types"
+
+const MatterportComponent = () => {
+
+  const [timer, setTimer] = useState(0)
+  const [mpSdk, setMpSdk] = useState<MpSdk>()
+
+  // SDKの取得（時間がかかる）
+  useEffect(() => {
+
+    const id = setInterval(() => {
+      const matterportViewer = document.querySelector<MatterportViewer>('matterport-viewer')
+      if (matterportViewer) {
+        matterportViewer.playingPromise
+          .then((sdk) => {
+            console.log(sdk)
+            setMpSdk(sdk)
+          })
+          .catch((e) => console.error(e))
+          .finally(() => { })
+        clearInterval(id)
+      } else {
+        setTimer(t => t + 1)
+      }
+    }, 1000)
+
+    return () => clearInterval(id);
+
+  }, [])
+
+  const [gltfComponent, setGltfComponent] = useState<MpSdk.Scene.IComponent>()
+  const [x, setX] = useState(0.5)
+  const [y, setY] = useState(2.5)
+  const [z, setZ] = useState(0.0)
+  const [isChecked, setChecked] = useState(false)
+
+  useEffect(() => {
+    if (gltfComponent && gltfComponent.inputs) {
+      gltfComponent.inputs.localPosition = {
+        x: x, y: y, z: z
+      }
+
+      gltfComponent.inputs.url = isChecked
+        ? 'https://cdn.jsdelivr.net/gh/mrdoob/three.js@dev/examples/models/gltf/Flamingo.glb'
+        : 'https://cdn.jsdelivr.net/gh/mrdoob/three.js@dev/examples/models/gltf/Parrot.glb'
+    }
+  }, [x, y, z, isChecked])
+
+  const reqIdRef = useRef<number>(0)
+  const [moveCounter, setMoveCounter] = useState(-1)
+
+  const move = () => {
+    setZ(z + 0.02)
+  }
+
+  function startMove() {
+    setMoveCounter(0)
+  }
+
+  useEffect(() => {
+    if (0 <= moveCounter && moveCounter <= 5000) {
+      reqIdRef.current = requestAnimationFrame(move)
+      setMoveCounter(moveCounter + 1)
+      if (moveCounter > 1000) {
+        setChecked(true)
+      } else {
+        setChecked(false)
+      }
+    } else {
+      cancelAnimationFrame(reqIdRef.current)
+    }
+  }, [moveCounter, x, y, z])
+
+  function add() {
+    if (mpSdk) {
+      mpSdk.Scene.createObjects(1)
+        .then(([sceneObject]) => {
+          // add a scene node for the fbx model
+          const gltfNode = sceneObject.addNode();
+
+          // adjust the position of the scene node
+          gltfNode.position.set(0, -0.68, 0);
+
+          // add the gltf loader component that loads a parrot model. Adjust the model's scale to make it fit inside the model.
+          const gltfComponent = gltfNode.addComponent('mp.gltfLoader', {
+            url: 'https://cdn.jsdelivr.net/gh/mrdoob/three.js@dev/examples/models/gltf/Parrot.glb',
+            localScale: {
+              x: 0.03,
+              y: 0.03,
+              z: 0.03,
+            },
+            localPosition: {
+              x: 0.5,
+              y: 2.5,
+              z: 0
+            }
+          });
+          setGltfComponent(gltfComponent)
+
+          // Add a path id 'gltfUrl' to the gltf component url property (not used in the this example).
+          sceneObject.addInputPath(gltfComponent, 'url', 'gltfUrl');
+
+          // add another scene node to contain the light objects.
+          const lightsNode = sceneObject.addNode();
+
+          // Add directional and ambient lights
+          const directionalLightComponet = lightsNode.addComponent('mp.directionalLight', {
+            color: { r: 0.7, g: 0.7, b: 0.7 },
+          });
+          lightsNode.addComponent('mp.ambientLight', {
+            intensity: 0.5,
+            color: { r: 1.0, g: 1.0, b: 1.0 },
+          });
+
+          // Add a path id 'ambientIntensity' to the intensity property of the directional light component.
+          // The path will be used to set the value later.
+          const ambientIntensityPath = sceneObject.addInputPath(directionalLightComponet, 'intensity', 'ambientIntensity');
+
+          // Start the scene object and its nodes.
+          sceneObject.start();
+        }
+        )
+    }
+
+  }
+
+  return (
+    <>
+      <div style={{ position: 'absolute', left: '804px' }}>
+        <button onClick={add}>追加</button>
+        <br></br>
+        X: <input type='number' step={0.1} value={x} onChange={(e) => setX((Number)(e.target.value))}></input>
+        <br></br>
+        Y: <input type='number' step={0.1} value={y} onChange={(e) => setY((Number)(e.target.value))}></input>
+        <br></br>
+        Z: <input type='number' step={0.1} value={z} onChange={(e) => setZ((Number)(e.target.value))}></input>
+        <br></br>
+        切り替え <input type='checkbox' checked={isChecked} onChange={(e) => setChecked(e.target.checked)}></input>
+        <br></br>
+        <button onClick={startMove}>移動開始</button>
+      </div>
+    </>
+  )
+}
+
+export default MatterportComponent
--- components/TwinMakerScene.tsx
+++ components/TwinMakerScene.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 
 import { TimeSeriesDataQuery } from "@iot-app-kit/core";
 import { SceneViewer } from '@iot-app-kit/scene-composer';
@@ -7,6 +7,7 @@ import { TwinMakerQuery, initialize } from '@iot-app-kit/source-iottwinmaker';
 import { S3SceneLoader } from '@iot-app-kit/source-iottwinmaker/dist/es/scene-module/S3SceneLoader';
 import { SceneMetadataModule } from '@iot-app-kit/source-iottwinmaker/dist/es/scene-module/SceneMetadataModule';
 import { aws_credentials, twinmaker_config } from '@/aws.config';
+import MatterportComponent from './MatterportComponent';
 
 
 interface twinMakerDataProps {
@@ -20,37 +21,28 @@ const sceneId = twinmaker_config.twinmaker_scene_id
 
 const TwinMakerScene = () => {
 
-  const [mounted, setmounted] = useState(false)
-
-  useEffect(() => {
-    const load = async () => {
-      try {
-        const mpSdk = await document.querySelector<any>('matterport-viewer')?.playingPromise
-        setmounted(true)
-      } catch (e) {
-        console.log(e)
-      }
-    }
-    load()
-  }, [])
-
   const twinMakerData: twinMakerDataProps | null = useMemo(() => {
     return initialize(workspaceId, {
       awsRegion: twinmaker_config.twinmaker_region,
       awsCredentials: aws_credentials
     })
-  }, [mounted])
+  }, [])    
 
 
   return (
     <>
-      {twinMakerData && mounted ? (
-        <SceneViewer
-          sceneLoader={twinMakerData.s3SceneLoader(sceneId)}
-          sceneMetadataModule={twinMakerData.sceneMetadataModule(sceneId)}
-          viewport={{ duration: '2h' }}
-          externalLibraryConfig={{ matterport: { assetBase: '/matterport' } }}
-        ></SceneViewer>
+      {twinMakerData ? (
+        <div>
+          <div style={{ width: '800px' }}>
+            <SceneViewer
+              sceneLoader={twinMakerData.s3SceneLoader(sceneId)}
+              sceneMetadataModule={twinMakerData.sceneMetadataModule(sceneId)}
+              viewport={{ duration: '2h' }}
+              externalLibraryConfig={{ matterport: { assetBase: '/matterport' } }}
+            ></SceneViewer>
+          </div>
+          <MatterportComponent></MatterportComponent>
+        </div>
       ) : <></>}
     </>
   )
